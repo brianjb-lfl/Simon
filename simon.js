d92160957d3e5d097addc976957b82146680182a
Brian Besaw <bjbesaw@outlook.com>
2017-07-29 23:40:13 -0400

    added detail

--- simon.js
+++ simon.js
@@ -1,52 +1,81 @@
-function simonMain(){
 
-  let simonSeq;
-  let gameInProg;
-  let userPlaying;
-  const colors = ["red", "blue", "yellow", "green"];
+let btnPressed = 'none';
 
 
-  startSimon();
-  userPlaying = true;
 
-  while (userPlaying) {
- 
-    console.log('\n *** STARTING NEW GAME');
-    gameInProg = true;
-    simonSeq=[];
-  
-    while(gameInProg){
-      simonSeq.push(colors[parseInt(Math.random()*4)]);
-
-      alert('LEVEL '+ simonSeq.length + ': demonstrating sequence');
-      console.log('\n LEVEL ' + simonSeq.length + '\n demonstrating sequence');
-      console.log(simonSeq);
-      document.getElementById("runLevel").innerHTML = 'Level ' + simonSeq.length;
-
-
-      if(prompt("Did user get it right?","<y or n>").toLowerCase() === 'y'){
-        console.log('user got it right');
-      }
-      else {
-        console.log('user got it wrong');
-        gameInProg = false;
-      }
-    }
-    
-    if(prompt('Game Over.  Play again?', '<y or n>').toLowerCase() === 'y'){
-      console.log('\n keep playing')
-    }
-    else {
-      userPlaying = false;
-      console.log('\n Thanks for playing!');
-    }
-
-  }
-
-}
 
 function startSimon(){
 	console.log('show landing page');
-	console.log('user clicks \"Play!\"');
-	alert('display landing - user clicks \"Play!\"')
+}
+
+function sessionRun(){
+  let sessionOn = true;
+
+  console.log('user clicked start');
+  console.log('*** NEW SESSION');
+
+  while(sessionOn){
+    gamePlay();
+    console.log('new game prompt');
+
+    if getBtn() = 'end'{
+      console.log('user opted to end session');
+      sessionOn = false;
+    }
+  }
+  alert('Thank you for playing.');
+  console.log('session end');
+}
+
+function gamePlay(){
+  let simonSeq=[];
+  let colors=['red', 'blue', 'yellow', 'green']
+  let gameOn = true;
+  let button;
+
+  console.log('*** NEW GAME');
+
+  while(gameOn){
+    simonSeq.push(colors[parseInt(Math.random()*4)]);
+    console.log('level ' + simonSeq.length + ': ' + simonSeq);
+
+    //*** replace this with sequence processing
+    button = getBtn();
+
+    switch(button){
+      case 'green':
+        //user successful
+        console.log('User succeeded on level ' + simonSeq.length)
+        break;
+      case 'red':
+        //user failed
+        console.log('User failed on level ' + simonSeq.length)
+        gameOn = false;
+        break;
+    }
+    console.log('game over')
+  }
+
+
+}
+
+
+
+function getBtn(){
+  let currBtn;
+
+  if(btnPressed === 'none'){
+    setTimeout(waitForBtn, 1000);
+  }
+  else{
+    conole.log('retrieved button press info.')
+    currBtn = btnPressed;
+    btnPressed = 'none';
+    return currBtn;
+  }
+}
+
+function btnProc(btnDesc){
+  console.log('Fielded button press: ' + btnDesc);
+  btnPressed = btnDesc;
 }
