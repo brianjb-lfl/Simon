01a34676056d2f2c10cf58a16b0028c8ce74a6b2
Brian Besaw <bjbesaw@outlook.com>
2017-07-30 21:19:04 -0400

    basic structure and flow tested sat

--- simon.js
+++ simon.js
@@ -1,81 +1,53 @@
+let simonSeq = [];
+let colors = ['green', 'red', 'blue', 'yellow'];
 
-let btnPressed = 'none';
-
-
-
-
-function startSimon(){
-	console.log('show landing page');
+function btnStart(){
+  // hide overlay
+  gameRound();
 }
 
-function sessionRun(){
-  let sessionOn = true;
-
-  console.log('user clicked start');
-  console.log('*** NEW SESSION');
-
-  while(sessionOn){
-    gamePlay();
-    console.log('new game prompt');
-
-    if getBtn() = 'end'{
-      console.log('user opted to end session');
-      sessionOn = false;
-    }
-  }
-  alert('Thank you for playing.');
-  console.log('session end');
+function gameRound(){
+  simonSeq.push(colors[parseInt(Math.random()*4)]);
+  document.getElementById("runLevel").innerHTML = "Level " + simonSeq.length;
+  document.getElementById("tempSimonSeq").innerHTML = simonSeq;       // *** remove this
 }
 
-function gamePlay(){
-  let simonSeq=[];
-  let colors=['red', 'blue', 'yellow', 'green']
-  let gameOn = true;
-  let button;
-
-  console.log('*** NEW GAME');
-
-  while(gameOn){
-    simonSeq.push(colors[parseInt(Math.random()*4)]);
-    console.log('level ' + simonSeq.length + ': ' + simonSeq);
-
-    //*** replace this with sequence processing
-    button = getBtn();
-
-    switch(button){
-      case 'green':
-        //user successful
-        console.log('User succeeded on level ' + simonSeq.length)
-        break;
-      case 'red':
-        //user failed
-        console.log('User failed on level ' + simonSeq.length)
-        gameOn = false;
-        break;
-    }
-    console.log('game over')
-  }
-
-
+function newGame() {
+  document.getElementById("postGame").innerHTML = "";
+  simonSeq = [];
+  gameRound();
 }
 
-
-
-function getBtn(){
-  let currBtn;
-
-  if(btnPressed === 'none'){
-    setTimeout(waitForBtn, 1000);
-  }
-  else{
-    conole.log('retrieved button press info.')
-    currBtn = btnPressed;
-    btnPressed = 'none';
-    return currBtn;
-  }
+function endSession() {
+  document.getElementById("postGame").innerHTML = "Thanks for playing!";
 }
 
-function btnProc(btnDesc){
-  console.log('Fielded button press: ' + btnDesc);
-  btnPressed = btnDesc;
+function btnPress(btnDesc){
+/*  sequence logic pending
+    placeholder process:
+    green = user was successful
+    other = user failed          */
+
+  console.log('button press fielded - ' + btnDesc);
+
+  switch(btnDesc) {
+    case 'green':
+      gameRound();
+      break;
+    
+    case 'red':
+    case 'blue':
+    case 'yellow':
+      document.getElementById("runLevel").innerHTML = "";
+      document.getElementById("tempSimonSeq").innerHTML = "";       // *** remove this
+      document.getElementById("postGame").innerHTML = "Game over - Play again?";
+      break;
+    case 'again':
+      newGame();
+      break;
+    case 'quit':
+      endSession();
+      break;
+  }
+
 }
